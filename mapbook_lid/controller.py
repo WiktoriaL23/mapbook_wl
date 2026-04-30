1cffb8a121cda408df8b7c83deb31d753016df8f
WiktoriaL23 <wiktoria.lipska@student.wat.edu.pl>
2026-04-30 13:42:22 +0200

    feat: add  update function

--- mapbook_lid/controller.py
+++ mapbook_lid/controller.py
@@ -1,14 +1,30 @@
+def read_data(users_data: list) -> None:
+    for user in users_data:
+        print(
+            f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
 
-def read_data(users_data:list)->None:
+
+def add_user(users_data: list) -> None:
+    name = input('Podaj imię: ')
+    location = input('Podaj lokalizację: ')
+    posts = int(input('Podaj liczbę postów: '))
+    usermessage = ['']
+    users_data.append({'username': name, 'location': location, 'posts': posts, 'usermessage': usermessage})
+
+
+def remove_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do usunięcia: ')
 
     for user in users_data:
-        print(f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
+        if user['username'] == name:
+            users_data.remove(user)
 
 
-def add_user(users_data:list)->None:
+def update_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do zmiany: ')
 
-    name=input('Podaj imię: ')
-    location=input('Podaj lokalizację: ')
-    posts=int(input('Podaj liczbę postów: '))
-    usermessage=['']
-    users_data.append({'username': name, 'location': location,'posts': posts,'usermessage': usermessage})
+    for user in users_data:
+        if user['username'] == name:
+            user['username'] = input('Podaj nowe imię: ')
+            user['location'] = input('Podaj nową lokalizację: ')
+            user['posts'] = int(input('Podaj liczbę postów: '))
