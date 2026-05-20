ed72a7b707f1dd848256d92353d1874ad162a4f8
WiktoriaL23 <wiktoria.lipska@student.wat.edu.pl>
2026-05-20 09:20:07 +0200

    feat: gui

--- main.py
+++ main.py
@@ -1,21 +1,91 @@
-from mapbook_lid.model import users
-from mapbook_lid.controller import read_data, add_user, remove_user, update_user
+from tkinter import *
 
-while True:
-    print('0 - zakończ program')
-    print('1 - wyświetl znajomych')
-    print('2 - dodaj znajomego')
-    print('3 - usuń znajomego')
-    print('4 - zmodyfikuj dane znajomego')
+root=Tk()
 
-    choose=input('wybierz opcje: ')
-    if choose =='0':
-        break
-    if choose == '1':
-        read_data(users[1:])
-    if choose == '2':
-        add_user(users)
-    if choose == '3':
-        remove_user(users)
-    if choose == '4':
-        update_user(users)
+root.title("Mapbook_WL")
+root.geometry("1024x760")
+
+# FRAME
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektu = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2)
+
+# RAMKA LISTA OBIEKTOW
+
+label_lista_obiektow = Label(ramka_lista_obiektow, text="Lista znajomych: ")
+listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
+
+button_pokaz_szczegoly_obiektu = Button(ramka_lista_obiektow, text="Pokaz szczegoly")
+button_usun_obiekt = Button(ramka_lista_obiektow, text="Usun")
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text="Edytuj")
+
+label_lista_obiektow.grid(row=0, column=0)
+listbox_lista_obiektow.grid(row=1, column=0)
+button_pokaz_szczegoly_obiektu.grid(row=2, column=0)
+button_usun_obiekt.grid(row=2, column=1)
+button_edytuj_obiekt.grid(row=2, column=2)
+
+#  RAMKA FORMULARZ
+
+label_formularz = Label(ramka_formularz, text="Formularz: ")
+label_imie = Label(ramka_formularz, text="Imie: ")
+label_nazwisko = Label(ramka_formularz, text="Nazwisko: ")
+label_liczba_postow = Label(ramka_formularz, text="Liczba postow: ")
+label_location = Label(ramka_formularz, text="Lokalizacja: ")
+
+label_formularz.grid(row=0, column=0)
+label_imie.grid(row=1, column=0)
+label_nazwisko.grid(row=2, column=0)
+label_liczba_postow.grid(row=3, column=0)
+label_location.grid(row=4, column=0)
+
+
+entry_imie = Entry(ramka_formularz)
+entry_nazwisko = Entry(ramka_formularz)
+entry_liczba_postow = Entry(ramka_formularz)
+entry_location = Entry(ramka_formularz)
+
+entry_imie.grid(row=1, column=1)
+entry_nazwisko.grid(row=2, column=1)
+entry_liczba_postow.grid(row=3, column=1)
+entry_location.grid(row=4, column=1)
+
+button_dodaj_uzytkownika = Button(ramka_formularz, text= "Dodaj użytkownika")
+button_dodaj_uzytkownika.grid(row=5, column=0, columnspan=2)
+
+#  SZCZEGOLY OBIEKTU
+
+label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text = "Szczegóły użytkownika")
+label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text = "Imię: ")
+label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text = "...")
+label_nazwisko_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text = "Nazwisko: ")
+label_nazwisko_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text = "...")
+label_liczba_postow_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text = "Liczba postów: ")
+label_liczba_postow_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text = "...")
+label_lokalizacja_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text = "Lokalizacja: ")
+label_lokalizacja_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text = "...")
+
+
+label_szczegoly_obiektu.grid(row=0, column=0)
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
+label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1)
+label_nazwisko_szczegoly_obiektu.grid(row=1, column=2)
+label_nazwisko_szczegoly_obiektu_wartosc.grid(row=1, column=3)
+label_liczba_postow_szczegoly_obiektu.grid(row=1, column=4)
+label_liczba_postow_szczegoly_obiektu_wartosc.grid(row=1, column=5)
+label_lokalizacja_szczegoly_obiektu.grid(row=1, column=6)
+label_lokalizacja_szczegoly_obiektu_wartosc.grid(row=1, column=7)
+
+
+
+
+
+
+
+
+
+root.mainloop()
